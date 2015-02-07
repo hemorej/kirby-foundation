4cb9cebdc51eba57618b59d97215a53a25a9d77b
j_a_ <jerome.xa88@gmail.com>
2015-02-07 00:29:12 -0500

    Added kudos to article template

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -8,8 +8,35 @@
 <?php echo js('assets/js/foundation/foundation.min.js') ?>
 <?php echo js('assets/js/foundation/foundation.interchange.js') ?>
 <?php echo js('assets/js/foundation/foundation.topbar.js') ?>
+<?php echo js('assets/js/vendor/kudos.js') ?>
+
 <script>
-	$(document).foundation();
+    $(function() {
+
+        $(document).foundation();
+
+        var kudos = $.ajax({url: "?kudos",async: false});
+        var parts = window.location.pathname.split('/');
+        uid = parts[parts.length-1] + parts[parts.length-2];
+
+        $(".num").html(kudos.responseText);
+        $("figure.kudoable").kudoable();
+
+        if(localStorage.getItem(uid) == 'true') {
+            $("figure.kudoable").addClass("complete");
+        }
+        $("figure.kudo").bind("kudo:added", function(e)
+        {
+            localStorage.setItem(uid, 'true');
+            var kudos = $.ajax({url: "?kudos=plus"});
+        });
+
+        $("figure.kudo").bind("kudo:removed", function(e)
+        {
+            var kudos = $.ajax({url: "?kudos=minus"});
+            localStorage.removeItem(uid);
+        });
+    });
 </script>
 
 </body>
--- site/templates/article.php
+++ site/templates/article.php
@@ -1,6 +1,24 @@
+<?php 
+
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+	$param = $_GET['kudos'];
+	$current = 0;
+	if($file = $page->file('kudos.md')){
+		$current = file_get_contents($page->file('kudos.md'));
+	}
+
+	if(null == $param){
+		echo $current;
+	}else{
+		$param == 'plus' ? ++$current : --$current;
+		file_put_contents ( $page->root().'/kudos.md', $current);
+	}
+	return;
+}
+?>
+
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
-
 
 <div class="row medium-space-top">		
 	<div class="small-12 small-centered medium-12 columns">
@@ -23,6 +41,17 @@
   				<img data-interchange="[<?php echo $medium; ?>, (default)], [<?php echo $small; ?>, (small)], [<?php echo $medium; ?>, (medium)], [<?php echo $large; ?>, (large)]" >
   				<noscript><img src="<?php echo $large; ?>"></noscript>
 			<?php } ?>
+
+			<p class="medium-space-top"></p>
+
+			<figure class="kudo kudoable" data-id="1">
+			        <a class="kudobject">
+			            <div class="opening">
+			            	<span style="margin-left:-80px" class="num">0</span> Kudos
+			                <div class="circle">&nbsp;</div>
+			            </div>
+			        </a>
+			</figure>
 
 			<p class="medium-space-top">
 				<?php if($page->tags()) : ?>
